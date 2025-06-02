18428e011d895e27f6ce10e6ef4a4f47d9578414
Bela Stoyan <git@be7a.de>
2025-06-02 22:49:36 +0200

    remove old deps & attempt to fix linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# When Redis-Stack modules are built the “readies” make-helper invokes
+set -x
+
+# When Redis-Stack modules are built the "readies" make-helper invokes
 # cmake $(CMAKE_FLAGS) …
 # (see deps/readies/mk/cmake.rules). 
 # All items in $(CMAKE_FLAGS) are passed to the shell unquoted.
 # the un-escaped semicolon in -DCMAKE_PROGRAM_PATH is interpreted by the shell as a command-separator
 # thus we drop the second ${PREFIX}/bin
-export CMAKE_ARGS=$(echo "$CMAKE_ARGS" | sed "s@-DCMAKE_PROGRAM_PATH=${BUILD_PREFIX}/bin;${PREFIX}/bin@-DCMAKE_PROGRAM_PATH=${BUILD_PREFIX}/bin@")
+export CMAKE_ARGS=$(echo "$CMAKE_ARGS" \
+  | sed "s@-DCMAKE_PROGRAM_PATH=${BUILD_PREFIX}/bin;${PREFIX}/bin@-DCMAKE_PROGRAM_PATH=${BUILD_PREFIX}/bin@" \
+  | sed -E 's@-DCMAKE_FIND_ROOT_PATH=([^;]*);([^ ]*)@-DCMAKE_FIND_ROOT_PATH=\1\\;\2@g')
 
 make PREFIX=$PREFIX BUILD_TLS=yes BUILD_WITH_MODULES=yes install
 
